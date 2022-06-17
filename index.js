420769f9e5db96235863de69a946f739ee349a04
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-06-17 01:25:01 -0300

    test

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import ModuleA from './App';
 import {name as appName} from './app.json';
 
-export default AppRegistry.registerComponent(appName, () => ModuleA);
-// export default ModuleA
+AppRegistry.registerComponent(appName, () => ModuleA);
+export default ModuleA
